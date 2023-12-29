a784d998fa1d83e9cf700206c121276962fb30cb
Tomas Vosicky <vosictom@fit.cvut.cz>
2023-12-30 00:43:04 +0100

    MNK: FEATURE: brand icons

--- clients/gisquick-web/vite.config.ts
+++ clients/gisquick-web/vite.config.ts
@@ -36,50 +36,8 @@ export default defineConfig(() => {
           theme_color: '#242424',
           icons: [
             {
-              src: './map/icons/android-chrome-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-            },
-            {
-              src: './map/icons/android-chrome-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-            },
-            {
-              src: './map/icons/android-chrome-maskable-192x192.png',
-              sizes: '192x192',
-              type: 'image/png',
-              purpose: 'maskable',
-            },
-            {
-              src: './map/icons/android-chrome-maskable-512x512.png',
-              sizes: '512x512',
-              type: 'image/png',
-              purpose: 'maskable',
-            },
-            {
-              src: './map/icons/apple-touch-icon-60x60.png',
-              sizes: '60x60',
-              type: 'image/png',
-            },
-            {
-              src: './map/icons/apple-touch-icon-76x76.png',
-              sizes: '76x76',
-              type: 'image/png',
-            },
-            {
-              src: './map/icons/apple-touch-icon-120x120.png',
-              sizes: '120x120',
-              type: 'image/png',
-            },
-            {
-              src: './map/icons/apple-touch-icon-152x152.png',
-              sizes: '152x152',
-              type: 'image/png',
-            },
-            {
-              src: './map/icons/apple-touch-icon-180x180.png',
-              sizes: '180x180',
+              src: './map/icons/android-chrome-144x144.png',
+              sizes: '144x144',
               type: 'image/png',
             },
             {
@@ -95,11 +53,6 @@ export default defineConfig(() => {
             {
               src: './map/icons/favicon-32x32.png',
               sizes: '32x32',
-              type: 'image/png',
-            },
-            {
-              src: './map/icons/msapplication-icon-144x144.png',
-              sizes: '144x144',
               type: 'image/png',
             },
             {
